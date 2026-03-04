750b61b773fba12fb6e6147d14d6eece2ebe5a47
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-03-04 18:56:29 +0530

    feat(auth): add logout routes

--- api/src/modules/auth/auth.routes.ts
+++ api/src/modules/auth/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { upload } from "../../middlewares/multer.midderware";
-import { login, register } from "./auth.controller";
+import { upload } from "../../middlewares/multer.middlerware";
+import { login, logout, register } from "./auth.controller";
+import { verifyAuth } from "../../middlewares/verifyAuth.middleware";
 
 const authRouter = express.Router();
 
 authRouter.route("/register").post(upload.single("avatar"), register);
 authRouter.route("/login").post(login);
+authRouter.route("/logout").post(verifyAuth, logout);
 
 export default authRouter;
