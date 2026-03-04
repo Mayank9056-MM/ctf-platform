e6950636e2c6af9975a795e2a77cc611af4a0f82
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-03-04 18:55:45 +0530

    feat(auth): implement verify auth middeware for only login user access api's

---
+++ api/src/middlewares/verifyAuth.middleware.ts
@@ -0,0 +1,46 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { config } from "../config/config";
+import { NextFunction, Request, Response } from "express";
+import User from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import logger from "../utils/logger";
+
+interface TokenPayload extends JwtPayload {
+  _id: string;
+}
+
+export const verifyAuth = async (
+  req: Request,
+  _: Response,
+  next: NextFunction
+) => {
+  const accessToken =
+    req.cookies.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", ""); // if sent from header/mobile
+
+  if (!accessToken) {
+    throw new ApiError(401, "Unathorized");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      accessToken,
+      config.ACCESS_TOKEN_SECRET
+    ) as TokenPayload;
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Unauthorized");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    logger.error("Error verifying access token", error);
+    console.log(error);
+    throw new ApiError(401, "Invalid access token");
+  }
+};
