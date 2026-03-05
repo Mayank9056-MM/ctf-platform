024897f6a3fe669004bd34c61c194f00d9297625
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-03-06 01:58:22 +0530

    feat(auth): add forgot-password and reset-password , update account details routes

--- api/src/modules/auth/auth.routes.ts
+++ api/src/modules/auth/auth.routes.ts
@@ -2,19 +2,34 @@ import express from "express";
 import { upload } from "../../middlewares/multer.middlerware";
 import {
   changeCurrentPassword,
+  currentUser,
+  forgotPassword,
   login,
   logout,
   refreshAccessToken,
   register,
+  resetPassword,
+  updateAccountDetails,
+  updateUserAvatar,
 } from "./auth.controller";
 import { verifyAuth } from "../../middlewares/verifyAuth.middleware";
 
 const authRouter = express.Router();
 
+// Public Routes
 authRouter.route("/register").post(upload.single("avatar"), register);
 authRouter.route("/login").post(login);
+authRouter.route("/forgot-password").post(forgotPassword);
+authRouter.route("/reset-password/:token").post(resetPassword);
+
+// Protected Routes
 authRouter.route("/logout").post(verifyAuth, logout);
 authRouter.route("/refresh-token").get(verifyAuth, refreshAccessToken);
 authRouter.route("/change-password").patch(verifyAuth, changeCurrentPassword);
+authRouter.route("/current-user").get(verifyAuth, currentUser);
+authRouter.route("/update-account").patch(verifyAuth, updateAccountDetails);
+authRouter
+  .route("/update-avatar")
+  .patch(verifyAuth, upload.single("avatar"), updateUserAvatar);
 
 export default authRouter;
