0d8e261486f87541ca635f3f8f4d06158f7e31e7
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-03-07 21:45:00 +0530

    feat(auth): define authentication routes

--- api/src/modules/auth/auth.routes.ts
+++ api/src/modules/auth/auth.routes.ts
@@ -26,10 +26,10 @@ authRouter.route("/login").post(authRateLimiter, login);
 authRouter.route("/forgot-password").post(forgotPassword);
 authRouter.route("/reset-password/:token").post(resetPassword);
 authRouter.route("/oauth").post(authRateLimiter, oauthLogin);
+authRouter.route("/refresh-token").post(refreshAccessToken);
 
 // Protected Routes
 authRouter.route("/logout").post(verifyAuth, logout);
-authRouter.route("/refresh-token").get(verifyAuth, refreshAccessToken);
 authRouter.route("/change-password").patch(verifyAuth, changeCurrentPassword);
 authRouter.route("/current-user").get(verifyAuth, currentUser);
 authRouter.route("/update-account").patch(verifyAuth, updateAccountDetails);
