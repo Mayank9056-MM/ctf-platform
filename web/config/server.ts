2134148e23ebf144596f8ba1d83ece6305b84b0f
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-04-07 17:44:52 +0530

    feat(config): add socket URL to server config

--- web/config/server.ts
+++ web/config/server.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 
 const serverSchema = z.object({
   API_URL: z.url(),
+  NEXT_PUBLIC_SOCKET_URL: z.string().min(1),
 });
 
 export const serverConfig = serverSchema.parse({
   API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
 });
