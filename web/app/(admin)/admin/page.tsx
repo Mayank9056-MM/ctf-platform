09c15148222d623ba7858a544d3bd19c08745596
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-04-09 21:38:31 +0530

    feat(admin-dashboard): implement initial admin dashboard page with stats, charts, and audit logs

---
+++ web/app/(admin)/admin/page.tsx
@@ -0,0 +1,11 @@
+import AdminDashboardPage from "@/modules/admin/components/adminDashboardPage";
+
+const page = () => {
+  return (
+    <div>
+      <AdminDashboardPage />
+    </div>
+  );
+};
+
+export default page;
